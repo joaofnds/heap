4dbbbb7718b58ff7129b4cb990f6e5721abf3f2d
João Fernandes <joaovitorfernandes2@gmail.com>
2017-02-15 15:14:44 -0200

    Initial commit

---
+++ heap.h
@@ -0,0 +1,82 @@
+
+/** Descritor da heap
+  */
+struct heap{
+   /* seu código aqui */
+};
+
+/** Retorna a posição do pai
+ * @param p ponteiro para descritor de heap
+ * @param i índice do nodo 
+ * @return índice do pai ou 0 se erro
+ */
+int heap_parent(struct heap * p, int i);
+
+/** Retorna a posição do filho à esquerda
+ * @param p ponteiro para descritor de heap
+ * @param i índice do nodo 
+ * @return índice do filho ou 0 se maior que máximo
+ */
+int heap_left(struct heap * p, int i);
+
+/** Retorna a posição do filho à direita
+ * @param p ponteiro para descritor de heap
+ * @param i índice do nodo 
+ * @return índice do filho ou 0 se maior que máximo
+ */
+int heap_right(struct heap * p, int i);
+
+/** Retorna o máximo de elementos que a heap comporta
+ * @param p ponteiro para descritor de heap
+ * @return índice do filho ou 0 se maior que máximo
+ */
+int heap_max(struct heap * p);
+/** Cria uma heap
+ * @param max número máximo de elementos
+ * @return  um descritor ou NULL
+ */
+struct heap * heap_create(int max);
+
+/* Insere um elemento no fim da heap
+ * @param p descritor de heap
+ * @param val elemento a ser inserido
+ * @return 1 se OK, 0 se erro
+ */
+int heap_insert(struct heap * p, int val);
+
+/* Retorna o mínimo de um heap
+ * @param p descritor de heap
+ * @return valor mínimo, ou zero se heap vazio
+ */
+int heap_minimum(struct heap *p);
+
+/* Retorna e remove o primeiro elemento  da heap
+ * @param p descritor da heap
+ * @return o elemento ou 0
+ */
+int heap_minimum_delete(struct heap * p);
+
+/* Remove um elemento da heap
+ * @param p descritor da heap
+ * @param val o valor do elemento a ser removido
+ * @return 1 se OK ou 0 se erro
+ */
+int heap_delete(struct heap * p, int val);
+
+/* Corrige uma subárvore
+ * @param p descritor da heap
+ * @param i a posição da árvore
+ * @return 1 se OK ou 0 se erro
+ */
+int heap_heapify(struct heap * p, int i);
+
+/* Retorna se a heap está vazia ou não
+ * @param p descritor de heap
+ * @return 1 se vazia, 0 se não
+ */
+ int heap_isempty(struct heap *p);
+
+/** Desaloca toda a heap
+  * @param p descritor da heap
+  */
+void heap_destroy(struct heap * p);
---
+++ test.c
@@ -0,0 +1,74 @@
+#include "simpletest.h"
+#include "heap.h"
+
+#define MAX 128
+
+
+void test_create(){
+	DESCRIBE("CRIA HEAP");
+	WHEN("Eu crio uma HEAP");
+
+
+	IF("Sem parâmetros");
+	THEN("Resultado deve ser ponteiro");
+	struct heap * p = heap_create(MAX);
+	isNotNull(p);
+
+	THEN("Deve ser vazia");
+	isEqual(heap_isempty(p),1);
+
+	heap_destroy(p);
+}
+
+void test_insert(){
+	int i;
+	struct heap * p = heap_create(MAX);
+
+	DESCRIBE("INSERE ELEMENTOS");
+	WHEN("Eu insiro elementos");
+	IF("Insiro um número razoável MAX");
+	THEN("Deve inserir todos");
+	for(i=1; i<=MAX; i++){
+		isEqual(heap_insert(p,i), 1);
+	}
+	THEN("Deve não estar vazia");
+	isEqual(heap_isempty(p),0);
+
+	THEN("Primeiro elemento deve ser 1");
+	isEqual(heap_minimum(p),1);
+
+	heap_destroy(p);
+}
+
+void test_heapify(){
+
+	int i, val;
+	struct heap * p = create(MAX);
+
+
+	DESCRIBE("HEAPIFY");
+	WHEN("Eu insiro elementos e chamo heapify");
+	int valores[]={10,9,8,7,6,5,4,3,2,1};
+
+	for(i=1; i<=10; i++){
+		heap_insert(p,i);
+		heap_heapify(p,heap_parent(i))
+	}
+	for(i=1 i<=10; i++){
+		THEN("Deve ter cabeça igual a i");
+		isEqual(heap_minimum_delete(p),i);
+	}
+
+	THEN("Deve ser vazia");
+	isEqual(heap_isempty(p),1);
+	destroy(p);
+}
+
+
+
+
+int main () {
+	test_create();
+	test_insert();
+	test_heapify();
+}
